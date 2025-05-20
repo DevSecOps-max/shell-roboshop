14fab0f64455b670d84bc712f454a5670fd2f757
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-20 22:32:44 +0530

    catalogue files

---
+++ catalogue.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y  &>>$LOG_FILE
+LOGICCHECK $? "Disabling default nodejs module"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+LOGICCHECK $? "Enabling nodejs:20 module"
+
+dnf install nodejs -y &>>$LOG_FILE
+LOGICCHECK $? "Installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+LOGICCHECK $? "Creating a roboshop system user"
+
+mkdir /app 
+LOGICCHECK $? "Creating a app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
+LOGICCHECK $? "Downloading application code to the created app directory"
+
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+LOGICCHECK $? "unzipping catalogue"
+
+npm install  &>>$LOG_FILE
+LOGICCHECK $? "installing dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+LOGICCHECK $? "Copying the catalogue service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue  &>>$LOG_FILE
+systemctl start catalogue
+
+LOGICCHECK $? "Starting catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+LOGICHECK $? "Installing mongodb client"
+
+mongosh --host mongodb.laddudevops.shop </app/db/master-data.js &>>$LOG_FILE
+
+
+
+
+
+
+
+
