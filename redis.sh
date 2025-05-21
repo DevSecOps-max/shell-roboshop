9baf40c6b35bbb47d43e2fc1deca0a6fc9725978
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-21 17:31:43 +0530

    new roboshop file

--- redis.sh
+++ redis.sh
@@ -36,14 +36,13 @@ VALIDATE(){
 }
 
 
-
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling the default redis version"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enabling the required version of redis:7"
 
-dnf install redis -y 
+dnf install redis -y  &>$LOG_FILE
 VALIDATE $? "Installing the Redis"
 
 #sed -i "s/127.0.0.1/0.0.0.0" /etc/redis/redis.conf
