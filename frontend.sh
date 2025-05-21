4f9f39f3386cd4066da5e286811a806adfef85dd
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-21 15:40:01 +0530

    modified roboshop file

---
+++ frontend.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+
+dnf module list nginx &>>$LOG_FILE
+VALIDATE $? "listing the nginx module"
+
+dnf module disable nginx -y &>>$LOG_FILE
+VALIDATE $? "Disabling the nginx  default module"
+
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "Enabling the nginx:1.24"
+
+dnf install nginx -y  &>>$LOG_FILE
+VALIDATE $? "Installing the nginx"
+
+systemctl enable nginx  &>>$LOG_FILE
+systemctl start nginx   
+VALIDATE $? "Enabling and starting the nginx"
+
+rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "Removing and default content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading the Frontend Content"
+
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip   &>>$LOG_FILE
+VALIDATE $? "Unzipping the frontend content"
+
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Remove the default nginx configuration content"
+
+cp $SCRIPT_DIR/nginx.conf  /etc/nginx/nginx.conf
+VALIDATE $? "Copying the nginx.conf"
+
+systemctl restart nginx 
+VALIDATE $? "restarting the nginx"
+
+
+
+
+
+
+
+
+
+
+
