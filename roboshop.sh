c436838613055c4b770c7d014e33ac59e8e8ccd4
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-21 13:01:09 +0530

    modified roboshop file

--- roboshop.sh
+++ roboshop.sh
@@ -3,8 +3,8 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0369590f05cb0ab10" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
-DOMAIN_NAME="daws84s.site" # replace with your domain
+ZONE_ID="Z0458537358V1D9MFMKRW" # replace with your ZONE ID
+DOMAIN_NAME="laddudevops.shop" # replace with your domain
 
 for instance in ${INSTANCES[@]}
 #for instance in $@
